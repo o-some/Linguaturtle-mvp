95f6158b00c3d83e6f31402cc333933a6bc93908
o-some <eleftherios@o-some.de>
2026-08-01 00:30:51 +0200

    chore(v2): refresh offline cache for modular app

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='linguaturtle-v5';
-const FILES=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
+const CACHE='linguaturtle-v6';
+const FILES=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./src/data/content.js','./src/core/store.js'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)))});
 self.addEventListener('activate',event=>{event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),self.clients.claim()]))});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))))});
