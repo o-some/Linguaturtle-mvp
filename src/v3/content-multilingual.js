0fd1a65e6db6c6bfea81f4197c7a9664d5f26ef2
o-some <eleftherios@o-some.de>
2026-08-01 13:44:31 +0200

    Merge English places and words into multilingual content

--- src/v3/content-multilingual.js
+++ src/v3/content-multilingual.js
@@ -1,12 +1,15 @@
 import { collections as baseCollections } from './content.js';
 import { greekCollections, greekWords } from './content-el.js';
+import { englishCollections, englishWords } from './content-en.js';
 
 export const collections = baseCollections.map(collection => ({
   ...collection,
   ...(greekCollections[collection.id] || {}),
+  ...(englishCollections[collection.id] || {}),
   words: collection.words.map(word => ({
     ...word,
     el: greekWords[word.id] || word.de,
+    en: englishWords[word.id] || word.de,
   })),
 }));
 
