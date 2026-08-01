2f3fd0768d9e8ce065092eb7b65885dbebbcf0ba
o-some <eleftherios@o-some.de>
2026-08-01 07:53:06 +0200

    refactor(core): add core public API

---
+++ src/v3/core/index.js
@@ -0,0 +1,6 @@
+export { createRouter, register, navigate, renderCurrent, setNotFound } from './router.js';
+export { getState, setState, patchState, subscribe, resetState, initialState } from './store.js';
+export { registerAction, bindActions, clearActions } from './events.js';
+export { speak, stopAudio, isAudioEnabled } from './audio.js';
+export { grantReward, spendShells, levelFromXp, levelProgress } from './rewards.js';
+export { migrateLegacyState } from './legacy-migration.js';
