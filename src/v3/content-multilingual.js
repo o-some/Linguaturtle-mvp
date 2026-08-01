721de70915e1a15fce085438649dae5e76a3c809
o-some <eleftherios@o-some.de>
2026-08-01 12:47:03 +0200

    feat: compose multilingual content catalog

---
+++ src/v3/content-multilingual.js
@@ -0,0 +1,15 @@
+import { collections as baseCollections } from './content.js';
+import { greekCollections, greekWords } from './content-el.js';
+
+export const collections = baseCollections.map(collection => ({
+  ...collection,
+  ...(greekCollections[collection.id] || {}),
+  words: collection.words.map(word => ({
+    ...word,
+    el: greekWords[word.id] || word.de,
+  })),
+}));
+
+export const allWords = collections.flatMap(collection =>
+  collection.words.map(word => ({ ...word, collection: collection.id }))
+);
